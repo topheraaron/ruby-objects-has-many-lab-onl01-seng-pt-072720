4eeccd5d4810eafe3b78229b2b2f3c1024aa7345
Christopher Arias <chrstphr.aaron@gmail.com>
2020-08-22 20:43:30 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -17,13 +17,17 @@ class Artist
   end
 
   def add_song(song)
-    song.artist = self
+    #song.artist = self
+    @songs << song_name
+    song_name.artist = self
+    @@song_count += 1
   end
 
   def add_song_by_name(song)
     song = Song.new(song)
     @songs << song
     song.artist = self
+    @@song_count += 1
   end
 
 
